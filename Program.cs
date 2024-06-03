578e2a36a75566bee96b883e5f1d64ba86f114a1
akhirrashad <akhirrashad@gmail.com>
2024-06-03 12:49:10 -0700

    41. More String Manipulation with String Formatting

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Resources;
+
+int num = 10;
+double price = 19.95;
+string name = "Frank";
+
+// Interpolation
+Console.WriteLine($"The number is {num}");
+// Concatenation
+Console.WriteLine("The number is " + num);
+// String formatting
+Console.WriteLine("This number is {0}", num);
+
+// String formatting with multiple variables
+Console.WriteLine("This number is {0}, and the price is {1}. and the name is {2}", num, price, name);
